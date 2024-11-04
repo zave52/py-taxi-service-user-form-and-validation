cf70ac428091cd91bcbee98ea72da0175dcd5363
zakhar <zaharsavchen@gmail.com>
2024-11-04 10:54:21 +0200

    fix flake8 errors

--- taxi/urls.py
+++ taxi/urls.py
@@ -47,8 +47,16 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path("cars/<int:pk>/assign/", DriverAssignView.as_view(), name="car-assign-driver"),
-    path("cars/<int:pk>/remove/", DriverRemoveView.as_view(), name="car-remove-driver"),
+    path(
+        "cars/<int:pk>/assign/",
+        DriverAssignView.as_view(),
+        name="car-assign-driver"
+    ),
+    path(
+        "cars/<int:pk>/remove/",
+        DriverRemoveView.as_view(),
+        name="car-remove-driver"
+    ),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
     path(
